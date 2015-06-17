93ddb86d9e0faec886b5af0d2614911d0d49c5c8
Mayank Patel <patel.mayank@outlook.com>
2015-06-17 14:22:34 -0400

    add module

---
+++ modules/module.js
@@ -0,0 +1,12 @@
+export function sum (x, y) {
+  return x + y;
+}
+export var pi = 3.141593;
+
+
+// export default {
+//   pi: 3.141593,
+//   sum(x,y) {
+//     return x+ y;
+//   }
+// };
---
+++ modules/start.js
@@ -0,0 +1,3 @@
+import {sum} from "./module.js";
+
+console.log(`10 + 20 is ${sum(10, 20)}`);
